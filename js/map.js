66bf6ab1e42410b1313928d5b4a583edb95e30b1
Andreeva Anastasia <32935561+aam-fd@users.noreply.github.com>
2018-09-14 19:17:04 +0300

    module 3 task 13 CR - brought constants to UPPER SNAKE_CASE

--- js/map.js
+++ js/map.js
@@ -11,10 +11,16 @@ var MIN_GUESTS = 1;
 var MAX_GUESTS = 10;
 var MIN_ROOMS = 1;
 var MAX_ROOMS = 5;
+// цены
+var MIN_PRICE = 1000;
+var MAX_PRICE = 1000000;
+// параметры булавки на карте
+var PIN_WIDTH = 65;
+var PIN_HEIGHT = 87;
 
-var avatarNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
+var AVATAR_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8];
 
-var titles = [
+var TITLES = [
   'Большая уютная квартира',
   'Маленькая неуютная квартира',
   'Огромный прекрасный дворец',
@@ -25,27 +31,27 @@ var titles = [
   'Неуютное бунгало по колено в воде'
 ];
 
-var types = [
+var TYPES = [
   'palace',
   'flat',
   'house',
   'bungalo'
 ];
 
-var typesDecsription = {
+var TYPES_DESCRIPTION = {
   palace: 'Квартира',
   flat: 'Дворец',
   house: 'Дом',
   bungalo: 'Бунгало'
 };
 
-var checkTimes = [
+var CHECK_TIMES = [
   '12:00',
   '13:00',
   '14:00'
 ];
 
-var otherFeatures = [
+var OTHER_FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -54,7 +60,7 @@ var otherFeatures = [
   'conditioner'
 ];
 
-var apartamentPhotos = [
+var APARTAMENT_PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
@@ -74,7 +80,7 @@ var getNoRepeatElement = function (arr) {
 var getSortRandomNumber = function () {
   return Math.random() - 0.5;
 };
-var mixApartamentPhotos = apartamentPhotos.sort(getSortRandomNumber);
+var mixApartamentPhotos = APARTAMENT_PHOTOS.sort(getSortRandomNumber);
 
 
 var getAdsDescriptions = function () {
@@ -82,26 +88,26 @@ var getAdsDescriptions = function () {
   for (var i = 0; i < ADS_NUMBER; i++) {
     ads.push({
       author: {
-        avatar: 'img/avatars/user0' + getNoRepeatElement(avatarNumbers) + '.png', // адреса изображений не повторяются
+        avatar: 'img/avatars/user0' + getNoRepeatElement(AVATAR_NUMBERS) + '.png',
       },
 
       offer: {
-        title: getNoRepeatElement(titles), // значения не должны повторяться
-        address: '', // строка, адрес предложения, например, "600, 350"
-        price: getRandomNumber(1, 1000) * 1000, // рандом цена от 1000 до 1000000, округленная до тысяч
-        type: types[getRandomNumber(0, types.length)], // рандом от 0 до 3, palace, flat, house или bungalo
-        rooms: getRandomNumber(MIN_ROOMS, MAX_ROOMS), // рандом 1 до 5
-        guests: getRandomNumber(MIN_GUESTS, MAX_GUESTS), // рандом от 1 до 10
-        checkin: checkTimes[getRandomNumber(0, checkTimes.length)], // рандом от 0 до 2
-        checkout: checkTimes[getRandomNumber(0, checkTimes.length)], // рандом от 0 до 2
-        features: otherFeatures,
-        description: 'Welcome to Tokyo!',
-        photos: mixApartamentPhotos, // массив из строк, расположенных в произвольном порядке
+        title: getNoRepeatElement(TITLES),
+        address: '',
+        price: getRandomNumber(MIN_PRICE, MAX_PRICE),
+        type: TYPES[getRandomNumber(0, TYPES.length)],
+        rooms: getRandomNumber(MIN_ROOMS, MAX_ROOMS),
+        guests: getRandomNumber(MIN_GUESTS, MAX_GUESTS),
+        checkin: CHECK_TIMES[getRandomNumber(0, CHECK_TIMES.length)],
+        checkout: CHECK_TIMES[getRandomNumber(0, CHECK_TIMES.length)],
+        features: OTHER_FEATURES,
+        description: '',
+        photos: mixApartamentPhotos,
       },
 
       location: {
-        x: getRandomNumber(MIN_X, MAX_X), // случайное число, координата x метки на карте.
-        y: getRandomNumber(MIN_Y, MAX_Y), // случайное число, координата y метки на карте от 130 до 630.
+        x: getRandomNumber(MIN_X, MAX_X),
+        y: getRandomNumber(MIN_Y, MAX_Y),
       }
     });
   }
@@ -119,17 +125,17 @@ var mapPinTemplate = document.querySelector('#pin')
     .querySelector('.map__pin');
 
 var getCorrectX = function (adsDescription) {
-  return adsDescription.location.x - 33;
+  return adsDescription.location.x - PIN_WIDTH / 2;
 };
 
 var getCorrectY = function (adsDescription) {
-  return adsDescription.location.y - 87;
+  return adsDescription.location.y - PIN_HEIGHT;
 };
 
 var getRenderMapPin = function (adsDescription) {
   var mapPin = mapPinTemplate.cloneNode(true);
   mapPin.classList.add('map__pin--main');
-  mapPin.style = 'left:' + getCorrectX(adsDescription) + 'px; top:' + getCorrectY(adsDescription) + 'px;'; // 33 87
+  mapPin.style = 'left:' + getCorrectX(adsDescription) + 'px; top:' + getCorrectY(adsDescription) + 'px;';
   mapPin.querySelector('img').src = adsDescription.author.avatar;
   mapPin.querySelector('img').alt = adsDescription.offer.title;
   mapPin.querySelector('img').height = 44;
@@ -155,9 +161,9 @@ var getRenderCardAds = function (adsDescription) {
   var cardAds = cardTemplate.cloneNode(true);
   cardAds.querySelector('.popup__avatar').src = adsDescription.author.avatar;
   cardAds.querySelector('.popup__title').textContent = adsDescription.offer.title;
-  cardAds.querySelector('.popup__text--address').textContent = 'город Токио, координаты: ' + adsDescription.location.x + ', ' + adsDescription.location.y + '.';
+  cardAds.querySelector('.popup__text--address').textContent = adsDescription.location.x + ', ' + adsDescription.location.y;
   cardAds.querySelector('.popup__text--price').textContent = adsDescription.offer.price + '₽/ночь';
-  cardAds.querySelector('.popup__type').textContent = typesDecsription[adsDescription.offer.type];
+  cardAds.querySelector('.popup__type').textContent = TYPES_DESCRIPTION[adsDescription.offer.type];
   cardAds.querySelector('.popup__text--capacity').textContent = adsDescription.offer.rooms + ' комнаты для ' + adsDescription.offer.guests + ' гостей';
   cardAds.querySelector('.popup__text--time').textContent = 'Заезд после ' + adsDescription.offer.checkin + ', выезд до' + adsDescription.offer.checkout;
   cardAds.querySelector('.popup__description').textContent = adsDescription.offer.description;
