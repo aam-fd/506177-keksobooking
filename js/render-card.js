56e5a013e22c24845fdba939413bd0d7d89bb074
Andreeva Anastasia <32935561+aam-fd@users.noreply.github.com>
2018-10-10 18:06:12 +0300

    m 5 t 2 added ad replacement

--- js/render-card.js
+++ js/render-card.js
@@ -2,19 +2,27 @@
 
 (function () {
 
-
   var mapFilterContainer = window.map.map.querySelector('.map__filters-container');
+  var mapCard = window.map.map.querySelector('.map__card');
+
   var renderAdCard = function (selectedAd) {
-    window.map.map.insertBefore(window.createCard(selectedAd), mapFilterContainer);
+    if (mapCard) {
+      window.map.map.replaceChild(window.createCard(selectedAd), mapCard);
+    } else {
+      window.map.map.insertBefore(window.createCard(selectedAd), mapFilterContainer);
+    }
   };
+
   var onPinClick = function (evt) {
     var clickedElement = evt.target.id;
     var selectedAd = window.data.adsDescriptions[clickedElement];
+
     renderAdCard(selectedAd);
   };
 
   window.renderCard = function () {
     var pinElements = document.querySelectorAll('.map__pin');
+
     for (var i = 1; i < pinElements.length; i++) {
       pinElements[i].addEventListener('click', onPinClick);
     }
