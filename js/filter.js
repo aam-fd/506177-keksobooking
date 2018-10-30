42bc7f3b7a5b8267f28ed01a0dc01fce151bacfe
Andreeva Anastasia <32935561+aam-fd@users.noreply.github.com>
2018-10-30 13:51:03 +0300

    m 7 t 1 rewrote the filter chain

--- js/filter.js
+++ js/filter.js
@@ -1,61 +1,130 @@
 'use strict';
 
 window.filter = (function () {
-  var Filter = {
-    'type': 'flat',
-    'price': '', // any, low, middle, high
-    'rooms': '',
-    'guests': '',
-    'features': []
-  };
 
-  // var Price = {
-  //   LOW: 10000,
-  //   HIGH: 50000,
-  // };
+  var maxRenderedPins = window.constants.MAX_RENDERED_PINS;
+  var lowPrice = window.constants.Price.LOWER_LIMIT;
+  var highPrice = window.constants.Price.HIGHER_LIMIT;
+  var lowValue = window.constants.ValueRange.LOW;
+  var middleValue = window.constants.ValueRange.MIDDLE;
+  var highValue = window.constants.ValueRange.HIGH;
+  var anyValue = window.constants.ValueRange.ANY;
+  var debounceInterval = window.constants.DEBOUNCE_INTERVAL;
 
-  var array;
-  var name;
-  var value;
+  var array = [];
   var callback;
 
-  var onFilterFormChange = function (evt) {
-    value = evt.target.value;
 
-    if (evt.target.name === 'features') {
-      name = evt.target.name;
-      Filter[name].push(value);
+  var filterForm = document.querySelector('.map__filters');
+
+  var typeFilter = function (object) {
+
+    var adType = object.offer.type;
+    var typeFilterValue = filterForm.querySelector('#housing-type')
+                                    .value;
+
+    if (typeFilterValue === anyValue) {
+      return true;
     } else {
-      name = evt.target.name.slice(8);
+      return adType === typeFilterValue;
+    }
+  };
 
-      if (name === 'rooms' || name === 'guests') {
-        Filter[name] = +value;
-      } else {
-        Filter[name] = value;
+  var priceFilter = function (object) {
+
+    var adPrice = object.offer.price;
+    var priceFilterValue = filterForm.querySelector('#housing-price')
+                                      .value;
+
+    if (priceFilterValue === lowValue) {
+      return adPrice < lowPrice;
+    } else if (priceFilterValue === middleValue) {
+      return adPrice > lowPrice && adPrice < highPrice;
+    } else if (priceFilterValue === highValue) {
+      return adPrice > highPrice;
+    } else {
+      return true;
+    }
+  };
+
+  var roomsFilter = function (object) {
+
+    var adRooms = object.offer.rooms;
+    var roomsFilterValue = filterForm.querySelector('#housing-rooms')
+                                      .value;
+
+    if (roomsFilterValue === anyValue) {
+      return true;
+    } else {
+      roomsFilterValue = +roomsFilterValue;
+      return adRooms === roomsFilterValue;
+    }
+  };
+
+  var guestsFilter = function (object) {
+
+    var adGuests = object.offer.guests;
+    var guestsFilterValue = filterForm.querySelector('#housing-guests')
+                                        .value;
+
+    if (guestsFilterValue === anyValue) {
+      return true;
+    } else {
+      guestsFilterValue = +guestsFilterValue;
+      return adGuests === guestsFilterValue;
+    }
+  };
+
+  var featuresFilter = function (object) {
+
+    var adFeatures = object.offer.features;
+    var featuresCollection = filterForm.querySelector('#housing-features')
+                                        .elements;
+    var featuresSelected = [];
+
+    for (var i = 0; i < featuresCollection.length; i++) {
+      if (featuresCollection[i].checked) {
+        featuresSelected.push(featuresCollection[i].value);
       }
     }
 
-    // console.log(Filter);
-    // console.log(name);
+    if (featuresSelected.length > 0) {
 
-    var choosenAds = array.filter(function (object) {
-      return object.offer.type === Filter.type;
-    });
+      var featuresIsContained = [];
 
-    if (Filter[name] === 'any') {
-      choosenAds = array;
+      featuresSelected.forEach(function (element) {
+        featuresIsContained.push(adFeatures.includes(element));
+      });
+
+      return !featuresIsContained.includes(false);
+    } else {
+      return true;
     }
 
-    // console.log(choosenAds);
-    callback(choosenAds);
   };
 
-  return function (filter, arr, cb) {
+  var getFilteredData = function () {
+    var filteredData = array.filter(typeFilter)
+                          .filter(priceFilter)
+                          .filter(roomsFilter)
+                          .filter(guestsFilter)
+                          .filter(featuresFilter)
+                          .slice(0, maxRenderedPins);
+
+    callback(filteredData);
+  };
+
+  var onFilterFormChange = window.debounce(getFilteredData, debounceInterval);
+
+  return function (arr, cb) {
 
     array = arr;
     callback = cb;
 
-    filter.addEventListener('change', onFilterFormChange);
+    getFilteredData();
+
+    filterForm.addEventListener('change', onFilterFormChange);
 
   };
+
 })();
