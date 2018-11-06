041efa0bc8f31b7216c0cd0d4df540e7b4939d48
Andreeva Anastasia <32935561+aam-fd@users.noreply.github.com>
2018-11-06 19:41:36 +0300

    final-check-1 added some constants

--- js/constants.js
+++ js/constants.js
@@ -64,8 +64,10 @@ window.constants = (function () {
 
     DEBOUNCE_INTERVAL: 500,
 
-    URL_FOR_POST: 'https://js.dump.academy/keksobooking',
-    URL_FOR_GET: 'https://js.dump.academy/keksobooking/data',
+    Url: {
+      POST: 'https://js.dump.academy/keksobooking',
+      GET: 'https://js.dump.academy/keksobooking/data',
+    },
 
     CodeStatus: {
       OK: 200,
@@ -77,6 +79,8 @@ window.constants = (function () {
       ESC: 27,
       ENTER: 13
     },
+
+    FILE_TYPES: ['gif', 'jpg', 'jpeg', 'png'],
   };
 
 })();
