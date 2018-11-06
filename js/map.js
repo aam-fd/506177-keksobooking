6cfce9148ebd2d1caf14831a10a9ae4c28469526
Andreeva Anastasia <32935561+aam-fd@users.noreply.github.com>
2018-11-06 19:43:46 +0300

    final-check-1 remove the data sending function

--- js/map.js
+++ js/map.js
@@ -2,6 +2,7 @@
 
 (function () {
 
+  var main = document.querySelector('main');
   var area = document.querySelector('.map');
   var mainPin = area.querySelector('.map__pin--main');
 
@@ -20,25 +21,33 @@
   var addClass = window.util.addClass;
   var fillInputValue = window.util.fillInputValue;
 
-  var makeDraggable = window.makeDraggable;
-
   var createCard = window.card.create;
   var createPin = window.pin.create;
-
+  var makeDraggable = window.makeDraggable;
+  var changeAvatar = window.avatar;
   var filter = window.filter;
+
+  var removeActivePin = function () {
+    var activePin = document.querySelector('.' + window.constants.ACTIVE_PIN);
+    if (activePin !== null) {
+      removeClass(activePin, window.constants.ACTIVE_PIN);
+    }
+  };
 
   var closeCard = function () {
     var card = document.querySelector('.map__card');
     if (card !== null) {
       area.removeChild(card);
     }
+    removeActivePin();
   };
 
   var onEscPressToCloseCardAd = function (evt) {
     if (evt.keyCode === window.constants.KeyCode.ESC) {
       closeCard();
+      removeActivePin();
+      document.removeEventListener('keydown', onEscPressToCloseCardAd);
     }
-    document.removeEventListener('keydown', onEscPressToCloseCardAd);
   };
 
   var onFilterFormToCloseCardAd = function () {
@@ -47,17 +56,15 @@
   };
 
   var renderCardAd = function (selectedAd) {
+    document.addEventListener('keydown', onEscPressToCloseCardAd);
+    filterForm.addEventListener('change', onFilterFormToCloseCardAd);
+
     var card = document.querySelector('.map__card');
     var ad = createCard(selectedAd, closeCard);
 
-    if (card !== null) {
-      area.replaceChild(ad, card);
-    } else {
+    return card !== null ?
+      area.replaceChild(ad, card) :
       area.appendChild(ad);
-    }
-
-    document.addEventListener('keydown', onEscPressToCloseCardAd);
-    filterForm.addEventListener('change', onFilterFormToCloseCardAd);
   };
 
   var deletePins = function () {
@@ -79,10 +86,7 @@
       var selectedAd = data[pin.id];
       renderCardAd(selectedAd);
 
-      var activePin = document.querySelector('.' + window.constants.ACTIVE_PIN);
-      if (activePin !== null) {
-        removeClass(activePin, window.constants.ACTIVE_PIN);
-      }
+      removeActivePin();
 
       addClass(pin, window.constants.ACTIVE_PIN);
     };
@@ -92,25 +96,6 @@
     }
 
     switchDisabled(filterForm, false);
-  };
-
-  var onSuccess = function (data) {
-
-    data.forEach(function (ad, index) {
-      ad.class = 'pin-' + index;
-    });
-
-    renderPins(data);
-    filter(data, renderPins);
-
-  };
-
-  var setActiveState = function () {
-    switchDisabled(formElements, false);
-    removeClass(area, window.constants.MAP_FADED);
-    removeClass(adForm, window.constants.FORM_DISABLED);
-    shiftMainPinCoordsToTail();
-    window.load(onSuccess);
   };
 
   var getMainPinCoords = function () {
@@ -134,20 +119,16 @@
   var onEnterPress = function (evt) {
     if (evt.keyCode === window.constants.KeyCode.ENTER) {
       setActiveState();
+      document.removeEventListener('keydown', onEnterPress);
     }
-
-    document.removeEventListener('keydown', onEnterPress);
   };
 
   var calculateCoords = function (element, elementSize) {
     var pinX = element.offsetLeft + elementSize.WIDTH / 2;
 
-    var pinY;
-    if (area.classList.contains(window.constants.MAP_FADED)) {
-      pinY = element.offsetTop + elementSize.WIDTH / 2;
-    } else {
-      pinY = element.offsetTop + elementSize.HEIGHT;
-    }
+    var pinY = area.classList.contains(window.constants.MAP_FADED) ?
+      element.offsetTop + elementSize.WIDTH / 2 :
+      element.offsetTop + elementSize.HEIGHT;
 
     return Math.round(pinX) + ', ' + Math.round(pinY);
   };
@@ -161,8 +142,6 @@
     mainPin.addEventListener('mouseup', onMainPinFirstMouseUp);
   };
 
-  setMainPinEventListener();
-
   var setInactiveState = function () {
     switchDisabled(formElements, true);
     addClass(area, window.constants.MAP_FADED);
@@ -172,15 +151,122 @@
     fillAddressByCalculatedCoords(mainPin, window.constants.MainPinSize);
   };
 
-  setInactiveState();
-  makeDraggable(mainPin, window.constants.MainPinSize,
-      areaPins, window.constants.Area, fillAddressByCalculatedCoords);
-
-  window.map = {
-    setInactiveState: setInactiveState,
-    closeCard: closeCard,
-    deletePins: deletePins,
-    setMainPinEventListener: setMainPinEventListener,
+  var resetForm = function () {
+    adForm.reset();
   };
+
+  var setInactivePage = function () {
+    setInactiveState();
+    closeCard();
+    deletePins();
+    resetForm();
+    setMainPinEventListener();
+  };
+
+  var getEventMessage = function (eventMessage) {
+    var template = document.querySelector('#' + eventMessage)
+                                  .content
+                                  .querySelector('.' + eventMessage);
+    return template.cloneNode(true);
+  };
+
+  var onSuccess = function () {
+
+    var success = getEventMessage('success');
+    main.appendChild(success);
+
+    var removeEventListener = function () {
+      document.removeEventListener('keydown', onEscPress);
+      document.removeEventListener('click', onClick);
+    };
+
+    var onEscPress = function (evt) {
+      if (evt.keyCode === window.constants.KeyCode.ESC) {
+        main.removeChild(success);
+        removeEventListener();
+        setInactivePage();
+      }
+    };
+
+    var onClick = function () {
+      main.removeChild(success);
+      removeEventListener();
+      setInactivePage();
+    };
+
+
+    document.addEventListener('click', onClick);
+    document.addEventListener('keydown', onEscPress);
+  };
+
+  var onError = function () {
+
+    var error = getEventMessage('error');
+    main.appendChild(error);
+
+    var errorButton = error.querySelector('.error__button');
+
+    var removeEventListener = function () {
+      errorButton.removeEventListener('click', onClick);
+      document.removeEventListener('keydown', onEscPress);
+      document.removeEventListener('click', onClick);
+    };
+
+    var onEscPress = function (evt) {
+      if (evt.keyCode === window.constants.KeyCode.ESC) {
+        main.removeChild(error);
+        removeEventListener();
+      }
+    };
+
+    var onClick = function () {
+      main.removeChild(error);
+      removeEventListener();
+    };
+
+    errorButton.addEventListener('click', onClick);
+    document.addEventListener('click', onClick);
+    document.addEventListener('keydown', onEscPress);
+  };
+
+  var onLoad = function (data) {
+
+    data.forEach(function (ad, index) {
+      ad.class = 'pin-' + index;
+    });
+
+    renderPins(data);
+    filter(data, renderPins);
+
+  };
+
+  var onAdFormSubmit = function (evt) {
+    evt.preventDefault();
+    window.load('', onSuccess, onError, 'POST', window.constants.Url.POST, new FormData(adForm));
+  };
+
+  var onAdFormReset = function () {
+    setInactivePage();
+  };
+
+  var fileUpload = document.querySelector('.ad-form__field input[type=file]');
+  var avatar = document.querySelector('.ad-form-header__preview img');
+
+  var setActiveState = function () {
+    switchDisabled(formElements, false);
+    removeClass(area, window.constants.MAP_FADED);
+    removeClass(adForm, window.constants.FORM_DISABLED);
+    shiftMainPinCoordsToTail();
+
+    window.load('json', onLoad, onError, 'GET', window.constants.Url.GET, '');
+    makeDraggable(mainPin, window.constants.MainPinSize,
+        areaPins, window.constants.Area, fillAddressByCalculatedCoords);
+    changeAvatar(fileUpload, avatar);
+    adForm.addEventListener('submit', onAdFormSubmit);
+    adForm.addEventListener('reset', onAdFormReset);
+  };
+
+  setMainPinEventListener();
+  setInactiveState();
 
 })();
